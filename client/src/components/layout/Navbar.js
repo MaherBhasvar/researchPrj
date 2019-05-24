ad41c4a183975bd37953ea629f8fee22e96a24d3
MaherBhasvar <maher.daiict@gmail.com>
2019-05-25 00:17:38 +0530

    Update 4

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,40 +1,34 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {logoutUser} from '../../actions/authActions';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { logoutUser } from '../../actions/authActions';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 
 class Navbar extends Component {
-    render () {
+    render() {
 
         let showbutton;
         console.log(this.props.auth.isAuthenticated)
-        if(this.props.auth.isAuthenticated) {
+        if (this.props.auth.isAuthenticated) {
             showbutton = (
                 <div>
-                    <button type="button" className="btn btn-danger" onClick={e => {e.preventDefault(); this.props.logoutUser();}}>
+                    <button type="button" className="btn btn-danger" onClick={e => { e.preventDefault(); this.props.logoutUser(); }}>
                         Logout
                     </button>
                 </div>
             );
         } else {
-            showbutton = (
-                <div>
-                    <button type="button" className="btn btn-outline-primary" onClick={e => {e.preventDefault();}}>
-                        <Link to='/'>Log In</Link>
-                    </button>
-                </div>
-            )
+            showbutton = null
         }
 
         return (
             <div className="Navbar">
                 <nav className="navbar sticky-top navbar-dark bg-dark">
-                <Link className="navbar-brand" to="/">
-                Research Survey
+                    <Link className="navbar-brand" to="/">
+                        Research Survey
                 </Link>
-                {showbutton}
+                    {showbutton}
 
                 </nav>
             </div>
@@ -45,13 +39,12 @@ class Navbar extends Component {
 Navbar.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
-  };
-  
-  const mapStateToProps = state => ({
+};
+
+const mapStateToProps = state => ({
     auth: state.auth
-  });
-  
-  export default connect(mapStateToProps, { logoutUser, })(
+});
+
+export default connect(mapStateToProps, { logoutUser, })(
     Navbar
-  );
-  
+);
